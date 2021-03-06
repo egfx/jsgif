90993dee6a8c009e56cc8fde66251de69cec5829
Ilya Berelson <segrafix@gmail.com>
2021-03-05 20:48:22 -0800

    Update GIFEncoder_WebWorker.js

--- GIFEncoder_WebWorker.js
+++ GIFEncoder_WebWorker.js
@@ -8,6 +8,7 @@ GIFEncoder_WebWorker = function(options) {
         this.frames = []
         this.num_threads = 8
         this.base_url = options.base_url
+        this.transparent = 0xffffff
     }
     
     var setRepeat = exports.setRepeat = function setRepeat(repeat) {
@@ -29,6 +30,10 @@ GIFEncoder_WebWorker = function(options) {
 
     var start = exports.start = function start() {
          return true;       
+    }
+    
+    var setTransparent = exports.setTransparent = function setTransparent(transparent) {
+         this.transparent = transparent
     }
     
     var finish_sync = exports.finish_sync = function finish_sync(cba) {
@@ -74,6 +79,7 @@ GIFEncoder_WebWorker = function(options) {
                 width: curr.canvas.width,
                 repeat: this.repeat,
                 delay:  this.delay,
+                transparent: this.transparent,
                 imageData: imdata.data.buffer//imarray.join(',')
             }
             window.x = curr
